eaf7f0ce186cef5ad8374a32a52efd2ab6103995
GoeminneSenne <Senne.goeminne@student.howest.be>
2026-03-01 22:31:31 +0100

    Added Imgui demo window to renderer

--- Minigin/Renderer.cpp
+++ Minigin/Renderer.cpp
@@ -45,12 +45,23 @@ void dae::Renderer::Init(SDL_Window* window)
 
 void dae::Renderer::Render() const
 {
+	ImGui_ImplSDLRenderer3_NewFrame();
+	ImGui_ImplSDL3_NewFrame();
+	ImGui::NewFrame();
+
+	//TODO for demonstration purposes only, remove this
+	//And replace it with a renderUI function
+	ImGui::ShowDemoWindow();
+
+	ImGui::Render();
+
 	const auto& color = GetBackgroundColor();
 	SDL_SetRenderDrawColor(m_renderer, color.r, color.g, color.b, color.a);
 	SDL_RenderClear(m_renderer);
 
 	SceneManager::GetInstance().Render();
 
+	ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), m_renderer);
 	SDL_RenderPresent(m_renderer);
 }
 
